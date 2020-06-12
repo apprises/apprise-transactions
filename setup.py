a800f3d7ec29e2f6a8f9fb76986ee12b4bba8fdd
apprises <apprises@tutanota.com>
2020-06-12 10:17:26 -0400

    Rolling to next version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="apprise-transactions",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     long_description=open("README.md").readlines(),
