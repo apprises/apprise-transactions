d676ff6176da7c6b2d311dcbf9cda72acdb71702
apprises <66576237+apprises@users.noreply.github.com>
2021-09-18 23:42:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="apprise-transactions",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     include_package_data=True,
     long_description=README,
